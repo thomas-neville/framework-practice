41a77c22e01318a79609e446453f7733b4763214
Cheri Allen <cherimarie@gmail.com>
2018-05-22 22:52:35 -0700

    Initial commit

---
+++ script.js
@@ -0,0 +1,18 @@
+var gators = [
+  {name: "Elvis Vessley",
+    hobbies: "surfing, dancing",
+    weight: 865,
+    likesIceCream: true,
+    imgSource: "https://img-aws.ehowcdn.com/877x500p/s3-us-west-1.amazonaws.com/contentlab.studiod/getty/10c413dcc4d444acbd6810f483028933"
+  },{name: "Little Britches",
+    hobbies: "shaking, eating fruit",
+    weight: 361,
+    likesIceCream: true,
+    imgSource: "http://nebula.wsimg.com/5df260927e75521ad0f18fae842e522e?AccessKeyId=00569247111AAAE64481&disposition=0&alloworigin=1"
+  },{name: "Wanda Snapson",
+    hobbies: "having parties",
+    weight: 712,
+    likesIceCream: false,
+    imgSource: "http://www.vaguebuttrue.com/images/1449775760-alligatorclimbsfenceWEBSITE.jpg"
+  }
+]
