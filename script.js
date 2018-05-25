37bfcadbad8bf365f4c05cb8f70ce71ec2d49b1f
Thomas Neville <tnev@uw.edu>
2018-05-24 19:19:31 -0700

    create component for header

--- script.js
+++ script.js
@@ -17,6 +17,12 @@ var gators = [
   }
 ]
 
+
+Vue.component('app-header', {
+  props: ['text'],
+  template: '<header><h1><img src="alligator-logo.png" alt="Alligator Logo">{{ text }}</h1></header>'
+})
+
 var app = new Vue({
   el: '#app',
   data: {
